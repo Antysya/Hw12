3a3a99110f31e436fe18669f6a6c45670ba8c735
Antysya <antysya@mail.ru>
2023-11-12 21:11:20 +0300

    Hw14

---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Models
+{
+    public class Product
+    {
+        public int Id { get; init; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
---
+++ ShopClient/ShopClient.cs
@@ -0,0 +1,31 @@
+﻿using Models;
+using System.Net.Http.Json;
+
+namespace ShopClient
+{
+    public class ShopClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _host;
+        public ShopClient(HttpClient httpClient, string host)
+        {
+            if (string.IsNullOrWhiteSpace(host))
+            {
+                throw new ArgumentException($"'{nameof(host)}' host не может быть пустым"); 
+            }
+            if(httpClient is null)
+            {
+                throw new ArgumentNullException(nameof(httpClient));    
+            }
+            _httpClient = httpClient;
+            _host = host;
+        }
+
+        public async Task<IReadOnlyList<Product>>GetProductsAsync()
+        {
+            var uri = $"{_host}/api/products";
+            var products = await _httpClient.GetFromJsonAsync<IReadOnlyList<Product>>(uri);
+            return products!;
+        }
+    }
+}
